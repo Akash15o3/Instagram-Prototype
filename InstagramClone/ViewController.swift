71dfb49cbacf9f441d78a990fd3fe9ea7fe22c36
Akash.Aggarwal <aaggarwal@c02dq8v4md6r.corp.intusurg.com>
2022-03-20 10:43:42 -0700

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Akash Aggarwal on 3/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
