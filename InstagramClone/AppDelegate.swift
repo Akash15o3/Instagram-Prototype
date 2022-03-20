63791d5220b8e43ccfdfaeb2c71b5aabe2f77252
Akash.Aggarwal <aaggarwal@c02dq8v4md6r.corp.intusurg.com>
2022-03-20 16:06:41 -0700

    Added Readme

--- InstagramClone/AppDelegate.swift
+++ InstagramClone/AppDelegate.swift
@@ -6,13 +6,19 @@
 //
 
 import UIKit
-
+import Parse
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "tcTUdPJQ3EkIoUNfObQFt84MaiWP53XyeJEb1fyg"
+                $0.clientKey = "x6DEit4p257vIA6MI3Ul50TzpUS70A98pf51UZN5"
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
         // Override point for customization after application launch.
         return true
     }
@@ -30,7 +36,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    
+    
 
 }
 
